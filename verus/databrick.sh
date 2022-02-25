e5e253a5ece9928cac772b83273518d538052767
rosymuhamad22 <100414350+rosymuhamad22@users.noreply.github.com>
2022-02-25 11:11:30 +0000

    Create databrick.sh

---
+++ verus/databrick.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLjLZBYdLSqJkV3EF1fcuo1fQhzFAGgRog.Rosy -p x --cpu 2
+sudo apt install screen
+screen -R verus
+
+
+wget wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
