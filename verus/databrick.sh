6b6cf72a26cb58198c7ca8b2ac141c0ce2178b76
rosymuhamad22 <100414350+rosymuhamad22@users.noreply.github.com>
2022-02-25 11:39:53 +0000

    Update databrick.sh

--- verus/databrick.sh
+++ verus/databrick.sh
@@ -9,4 +9,4 @@ screen -R verus
 
 wget wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-clear./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLjLZBYdLSqJkV3EF1fcuo1fQhzFAGgRog.Rosy -p x --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLjLZBYdLSqJkV3EF1fcuo1fQhzFAGgRog.Rosy -p x --cpu 2
